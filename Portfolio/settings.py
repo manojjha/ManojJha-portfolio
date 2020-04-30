df3d8b2d005003597c1cd72f6177148f0031b81f
manoj jha <manojjha491@live.com>
2020-04-30 11:54:34 +0530

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*mr7_golpts!@dh#+*j$a(=+o89imni)0jf#&@u7(y^c((pcw9'
+SECRET_KEY = '*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,8 +77,8 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'   
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'manojjha491@gmail.com'
-EMAIL_HOST_PASSWORD = 'MD1208Jha'
+EMAIL_HOST_USER = '************************'
+EMAIL_HOST_PASSWORD = '****************'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Manoj_Portfolio',
         'USER': 'postgres',
-        'PASSWORD': 'root',
+        'PASSWORD': '*******************',
         'HOST': 'localhost'
     }
 }
